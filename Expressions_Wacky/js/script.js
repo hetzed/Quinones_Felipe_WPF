dc716ba100c39685a68aa96128d164476990f5f4
hetzed <felipe.dj.quinones@gmail.com>
2014-09-11 22:15:17 -0400

    Adding Array
    var cars, arrays.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,5 +4,9 @@
  Assignment: Expressions | Expressions Wacky
  */
 
+//This is the crazy lottery, consist of selecting a number and the system will match that number to something and give to the user
+alert("Welcome to the crazy lottery, please press OK and start winning!");
 
+//First establish the array that will be the different car models
+var cars = ["Mazda", "Toyota", "Audi", "Mercedes Benz", "BMW", "Aston Marti", "Infinity", "Honda", "Lexus", "Range Rover"];
 
