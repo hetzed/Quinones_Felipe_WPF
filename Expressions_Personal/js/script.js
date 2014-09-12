e9e0066aaf4c7d87d2416c2f9f13d5915d2bc71e
hetzed <felipe.dj.quinones@gmail.com>
2014-09-11 22:10:26 -0400

    Adding calculations
    calculations for socSec, medicare, afterTaxes

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -27,7 +27,27 @@ var payRate = prompt("What is your pay rate");
 //Console.log out Total hours worked
 console.log(payRate);
 
+// Calculate how much salary will get
+var weekSalary = totalHours * payRate;
+//Console.log out salary
+console.log("You\'ll get this week the amount of $" + weekSalary + " dollars before taxes.");
 
+// Calculate how much salary after taxes
+
+// Variable for social security
+var socSec = 0.062 * weekSalary;
+//Console.log out social security
+console.log(socSec);
+
+// Variable for medicare
+var mediCare = 0.0145 * weekSalary;
+//Console.log out medicare
+console.log(mediCare);
+
+// Variable that hold the calculation for after taxes
+var afterTaxes = Math.round((weekSalary - socSec - mediCare)*100)/100; //round the result to two decimals
+//Console.log out afterTaxes
+console.log("After paying your social security and medicare you\'ll get the amount of $" + afterTaxes + " dollars.");
 
 
 
