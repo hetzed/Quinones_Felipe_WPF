5fb24ad0179db3c978e3d5fedd2032c2f81bac4e
hetzed <felipe.dj.quinones@gmail.com>
2014-09-11 22:08:40 -0400

    Adding variables
    variables for personal daysWorked, hoursWorked, totalHours, payRate.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,7 +4,28 @@
  Assignment:Expressions | Expressions Personal
  */
 
+// How much money I'll make in a week
+alert("How much you are going to make this Week?");
 
+// Variable for days worked
+var daysWorked = prompt("How many days you\'ll work this week"); //The number of days that the person is gonna work in the week
+//Console.log out daysWorked
+console.log(daysWorked);
+
+// Variable for hours worked
+var hoursWorked = prompt("How many hours per day are you going to work?");
+//Console.log out hoursWorked
+console.log(hoursWorked);
+
+// Variable for the total of hours worked
+var totalHours = daysWorked * hoursWorked; //Total hours worked
+//Console.log out Total hours worked
+console.log(totalHours);
+
+// Variable for the pay rate
+var payRate = prompt("What is your pay rate");
+//Console.log out Total hours worked
+console.log(payRate);
 
 
 
