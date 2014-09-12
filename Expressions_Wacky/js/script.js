ff1ba18228dba11ae5eb313987a7375a5e25556c
hetzed <felipe.dj.quinones@gmail.com>
2014-09-11 22:16:19 -0400

    Adding var and increment.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,3 +10,10 @@ alert("Welcome to the crazy lottery, please press OK and start winning!");
 //First establish the array that will be the different car models
 var cars = ["Mazda", "Toyota", "Audi", "Mercedes Benz", "BMW", "Aston Marti", "Infinity", "Honda", "Lexus", "Range Rover"];
 
+//Let the user select the number between 0 to 9
+var number = Number(prompt("Select a number from 0 to 9"));
+console.log(number); //Console log out the number selected by the user
+
+//Increment the number selected by the user
+number = ++number;
+console.log(number); //Console log out the number incremented
