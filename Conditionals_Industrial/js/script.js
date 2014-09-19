306ba4e809762e8a9f36f2926d2a90dcaf21ded2
hetzed <felipe.dj.quinones@gmail.com>
2014-09-18 23:30:25 -0400

    Adding script.js
    Adding first variables

---
+++ Conditionals_Industrial/js/script.js
@@ -0,0 +1,13 @@
+/*
+ Felipe Quiñones
+ WPF 1409 Section 01
+ Assignment:Conditionals | Conditional Industry
+ */
+
+// Test what day had the best service level
+
+//The user enters the info for the first variables
+var callsMon = prompt("How many calls the call center received on Monday?");
+var callsAIT_Mon = prompt("How many calls were answered in time?");
+
+
