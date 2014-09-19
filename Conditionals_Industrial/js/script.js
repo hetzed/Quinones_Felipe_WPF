b4103586f6e30746b9dda49c1c7ada0488483dc4
hetzed <felipe.dj.quinones@gmail.com>
2014-09-18 23:34:58 -0400

    Adding additional variables
    Calls for Tuesday and How may were in time

--- Conditionals_Industrial/js/script.js
+++ Conditionals_Industrial/js/script.js
@@ -12,3 +12,10 @@ var callsAIT_Mon = prompt("How many calls were answered in time?");
 
 console.log(callsMon + " calls on Monday"); //Console log calls received on Monday
 console.log(callsAIT_Mon + " calls in time Monday"); //Console log calls answered in time on Monday
+
+////The user enters the info for the next variables
+var callsTues = prompt("How many calls the call center received on Tuesday?");
+var callsAIT_Tues = prompt("How many calls were answered in time?");
+
+console.log(callsTues + " calls on Tuesday"); //Console log calls received on Tuesday
+console.log(callsAIT_Tues + " calls in time Tuesday"); //Console log calls answered in time on Tuesday
