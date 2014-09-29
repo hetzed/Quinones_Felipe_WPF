903bac97b2724a922370395a9c53e3938f634549
hetzed <felipe.dj.quinones@gmail.com>
2014-09-29 17:01:31 -0400

    Function
    writing the function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -20,3 +20,12 @@ while (! (dd > 0)){
 
 var oPlit = 33.8; //var ounces per liter
 var onzas = 16; //var ounces
+
+//Set Up totals
+var totals = calcLiters (nOfGymmers, onzas, dd, oPlit);
+
+//Function
+function calcLiters (nOfGymmers, onzas, dd, oPlit) {
+    var liters = (nOfGymmers * onzas * dd) / (oPlit);
+    return liters;
+}
