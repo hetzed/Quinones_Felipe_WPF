507f302a3b7641b6fdd46b577bfc341a4495d63f
hetzed <felipe.dj.quinones@gmail.com>
2014-09-29 16:47:19 -0400

    set up vars

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -4,3 +4,11 @@
  Assignment: Functions | Functions Industrial
  */
 
+//Water for the gym
+
+var calcG, nOfGymmers, onzas, dd, oPG;
+//validates null
+while(! (nOfGymmers > 0)){
+    nOfGymmers = prompt(" Tell how many partners goes to gym with you?", " ");
+    console.log(nOfGymmers)//console that out
+}
