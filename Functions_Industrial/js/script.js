07bd9c196a1666e45ae878f2bebddddbc290783f
hetzed <felipe.dj.quinones@gmail.com>
2014-09-29 16:51:32 -0400

    Establishing the while s

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -8,7 +8,12 @@
 
 var calcG, nOfGymmers, onzas, dd, oPG;
 //validates null
-while(! (nOfGymmers > 0)){
-    nOfGymmers = prompt(" Tell how many partners goes to gym with you?", " ");
-    console.log(nOfGymmers)//console that out
+while (! (nOfGymmers > 0)){
+    nOfGymmers = prompt("Tell how many partners goes to gym with you?", " ");
+    console.log(nOfGymmers); //console that out
+}
+
+while (! (dd > 0)){
+    dd = prompt("Tell how many days you need water for gym?", " ");
+    console.log(dd); //console that out
 }
